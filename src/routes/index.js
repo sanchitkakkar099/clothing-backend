77e37a34c01d2de3f756de19884da1e410730453
Dheeraj Kumar <{dheerajpanapurlanga@gmail.com}>
2024-04-23 10:33:07 +0530

    some changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,5 @@
 const router = require("express").Router();
 const adminroutes = require("./admin");
-const staffroutes = require("./staff")
-const categoryRoutes = require("./category");
-const bloodTestRoutes = require("./bloodTest")
-const customBloodTestRoutes = require("./customBloodTest")
-const quoteRoutes = require("./quote");
 
 router.use("/admin", adminroutes);
-router.use("/staff",staffroutes);
-router.use("/category", categoryRoutes);
-router.use("/bloodtest", bloodTestRoutes);
-router.use("/custombloodTest", customBloodTestRoutes);
-router.use("/quote", quoteRoutes);
 module.exports = router;
