205aa3d8543e2354f8e3f9dadfa6a3b993bc85cb
saurav689 <saurav.singh@xava.co.in>
2024-12-17 13:11:48 +0530

    admin auth remove from order fetch

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const ordersController = require("../controllers/orders.controller");
 const {AdminAuth} = require("../middleware/auth")
-router.get("/list",AdminAuth,ordersController.fetchOrdersController);
+router.get("/list",ordersController.fetchOrdersController);
 
 
 module.exports = router
